5cecb32b90f3b5fd59eb38ebdf459a0097bc1bba
Adam Wespiser <adamwespiser@Adams-MacBook-Pro.local>
2019-05-22 13:12:17 -0400

    add .../internal as a route which does not show course recs

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ app = Flask(__name__, template_folder = "./templates")
 
 @app.route('/')
 def index():
-    return render_template('home.html', my_list=range(1,41))
+    return render_template('home.html', my_list=range(1,41), show_course_rec="True")
+
+@app.route('/internal')
+def index_norec():
+    return render_template('home.html', my_list=range(1,41), show_course_rec="False")
+
+
+
 @app.route('/img/<path:path>')
 def send_img(path):
     return send_from_directory('img', path)
@@ -20,34 +27,16 @@ def send_math(path):
 def success():
     try:
         if request.method == 'POST':
+            show_course_rec = True if request.form.get('show_course_rec') == "True" else False
+            print(f' show course rec {show_course_rec}')
             obj = run_analysis(request.form)
-            tables = {
-                    'm_diff'    : obj.get('math_difficulty'),
-                    'm_concept' : obj.get('math_concepts'),
-                    'r_diff'    : obj.get('reading_difficulty'),
-                    'r_concept' : obj.get('reading_concepts'),
-                    'w_diff'    : obj.get('writing_difficulty'),
-                    'w_concept' : obj.get('writing_concepts'),
-                    'r_explain' : obj.get('reading_explain'),
-                    'w_explain' : obj.get('writing_explain'),
-                    'm_explain' : obj.get('math_explain'),
-                    'm_improve' : obj.get('math_improve'),
-                    'v_improve' : obj.get('verbal_improve'),
-                    'v_best_concepts' : obj.get('verbal_best_concepts'),
-                    'm_best_concepts' : obj.get('math_best_concepts'),
-                    'v_worst_concepts' : obj.get('verbal_worst_concepts'),
-                    'm_worst_concepts' : obj.get('math_worst_concepts'),
-                    'm_improve_stmt' : obj.get('math_improve_stmt'),
-                    'v_improve_stmt' : obj.get('verbal_improve_stmt'),
-            }
-
+            tables = build_tables(obj)
             verbal_plot = plot_verbal(obj)
             math_plot = plot_math(obj)
             math_improve_plot = plot_total_miss_barchart(obj, "math_concept_plot", "Math")
             verbal_improve_plot = plot_total_miss_barchart(obj, "verbal_concept_plot", "Verbal")
             reading_improve_plot = plot_total_miss_barchart(obj, "reading_concept_plot", "Reading")
             writing_improve_plot = plot_total_miss_barchart(obj, "writing_concept_plot", "Writing")
-            #print(obj.get('math_concepts'))
             return render_template('result.html',
                     table = tables,
                     obj = obj,
@@ -59,12 +48,36 @@ def success():
                     writing_improve_plot = writing_improve_plot,
                     math_improve_plot = math_improve_plot,
                     verbal_plot = verbal_plot,
-                    math_plot = math_plot)
+                    math_plot = math_plot,
+                    show_rec = show_course_rec)
 
         else:
             pass
     except Exception as e:
         return render_template('500.html', error = str(e))
+
+
+def build_tables(obj):
+    tables = {
+            'm_diff'    : obj.get('math_difficulty'),
+            'm_concept' : obj.get('math_concepts'),
+            'r_diff'    : obj.get('reading_difficulty'),
+            'r_concept' : obj.get('reading_concepts'),
+            'w_diff'    : obj.get('writing_difficulty'),
+            'w_concept' : obj.get('writing_concepts'),
+            'r_explain' : obj.get('reading_explain'),
+            'w_explain' : obj.get('writing_explain'),
+            'm_explain' : obj.get('math_explain'),
+            'm_improve' : obj.get('math_improve'),
+            'v_improve' : obj.get('verbal_improve'),
+            'v_best_concepts' : obj.get('verbal_best_concepts'),
+            'm_best_concepts' : obj.get('math_best_concepts'),
+            'v_worst_concepts' : obj.get('verbal_worst_concepts'),
+            'm_worst_concepts' : obj.get('math_worst_concepts'),
+            'm_improve_stmt' : obj.get('math_improve_stmt'),
+            'v_improve_stmt' : obj.get('verbal_improve_stmt'),
+    }
+    return tables
 
 
 if __name__ == '__main__':
